2245c6849c6c906885a9e91d2784e7744e45626d
NabilHY <nabil.hayun@gmail.com>
2026-06-11 15:06:19 +0100

    Switch to using compose along with ansible staritng with ansible/compose building the mariadb container

--- roles/compose/files/mariadb/entrypoint.sh
+++ roles/compose/files/mariadb/entrypoint.sh
@@ -2,26 +2,6 @@
 
 echo "Starting MariaDB..."
 
-get_secret() {
-    local var_name="$1"
-    local secret_name="$2"
-    local file_path="/run/secrets/$secret_name"
-
-    if [ -f "$file_path" ]; then
-        export "$var_name"=$(cat "$file_path")
-        echo "LOG: Loaded $var_name from /run/secrets/$secret_name"
-    else
-        echo "FATAL: Missing secret file at $file_path"
-        exit 1
-    fi
-}
-
-get_secret "MYSQL_ROOT_PASSWORD" "wp_db_pass"
-get_secret "WORDPRESS_DB_NAME" "db_name"
-get_secret "WORDPRESS_DB_USER" "wp_admin_user"
-get_secret "WORDPRESS_DB_PASSWORD" "wp_db_pass"
-
-
 if [ ! -d "/var/lib/mysql/mysql" ]; then
     echo "Db Init"
     mariadb-install-db --user=mysql --datadir=/var/lib/mysql --skip-test-db
