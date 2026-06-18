ff37aceb6aabc73e9a617fe828ad3cb3df294b22
NabilHY <nabil.hayun@gmail.com>
2026-06-18 16:47:27 +0100

    convert phpmyadmin container to manual built image

--- roles/compose/files/phpmyadmin/init.sh
+++ roles/compose/files/phpmyadmin/init.sh
@@ -1,4 +1,22 @@
 #!/bin/sh
+
 set -e
+
+mkdir -p /run/lighttpd
+
+cat >/var/www/localhost/htdocs/config.inc.php <<'EOF'
+  <?php
+  $cfg['blowfish_secret'] = 'a8F3kLmQ9xRvTuWyNpZcBdEjHsIoGn01';
+  $i = 0;
+  $i++;
+  $cfg['Servers'][$i]['auth_type']       = 'cookie';
+  $cfg['Servers'][$i]['host']            = getenv('PMA_HOST') ?: 'mariadb';
+  $cfg['Servers'][$i]['port']            = getenv('PMA_PORT') ?: '3306';
+  $cfg['Servers'][$i]['connect_type']    = 'tcp';
+  $cfg['Servers'][$i]['compress']        = false;
+  $cfg['Servers'][$i]['AllowNoPassword'] = false;
+EOF
+
+echo "lightpd server started !"
 
 exec "$@"
