629e9f3a0dfc52a03769ef81d625849ff88ee249
NabilHY <nabil.hayun@gmail.com>
2026-06-22 12:46:26 +0100

    Fix health checks on docker compose and enforce compose rebuild on playbook run

--- roles/compose/files/mariadb/entrypoint.sh
+++ roles/compose/files/mariadb/entrypoint.sh
@@ -3,9 +3,9 @@
 echo "Starting MariaDB..."
 
 if [ ! -d "/var/lib/mysql/mysql" ]; then
-    echo "Db Init"
-    mariadb-install-db --user=mysql --datadir=/var/lib/mysql --skip-test-db
-    mariadbd --user=mysql --bootstrap << EOF
+  echo "Db Init"
+  mariadb-install-db --user=mysql --datadir=/var/lib/mysql --skip-test-db
+  mariadbd --user=mysql --bootstrap <<EOF
 FLUSH PRIVILEGES;
 ALTER USER 'root'@'localhost' IDENTIFIED BY '$MYSQL_ROOT_PASSWORD';
 CREATE DATABASE IF NOT EXISTS \`$WORDPRESS_DB_NAME\`;
@@ -13,10 +13,9 @@ CREATE USER IF NOT EXISTS '$WORDPRESS_DB_USER'@'%' IDENTIFIED BY '$WORDPRESS_DB_
 GRANT ALL PRIVILEGES ON \`$WORDPRESS_DB_NAME\`.* TO '$WORDPRESS_DB_USER'@'%';
 FLUSH PRIVILEGES;
 EOF
-    echo "Database Initialized"
+  echo "Database Initialized"
 fi
 
 echo "Starting $@"
 
 exec "$@"
-
